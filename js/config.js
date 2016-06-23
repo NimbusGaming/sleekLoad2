b6a0ee3180f76df43bfb54b06c16d34db81366dc
perlazombie <zombieperla@gmail.com>
2016-06-22 23:23:40 -0700

    Wonderland

---
+++ js/config.js
@@ -0,0 +1,127 @@
+﻿/*    _           _    _                     _ 
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.05
+*/
+
+/*URL for the server logo
+Leave it blank to use text instead*/
+var l_serverImage = "images/Wonderland.png";
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "sleekLoad 2";
+
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = false;
+
+/*Video background
+Avaiable options:
+"backgrounds/videos/aurora.webm"
+"backgrounds/videos/bluefall.webm"
+"backgrounds/videos/bluetilefloor.webm"
+"backgrounds/videos/bluewhitecircles.webm"
+"backgrounds/videos/bokehlens.webm"
+"backgrounds/videos/cleanbokeh.webm"
+"backgrounds/videos/coldnight.webm"
+"backgrounds/videos/colorfulburst.webm"
+"backgrounds/videos/dancingstars.webm"
+"backgrounds/videos/dropsflares.webm"
+"backgrounds/videos/focusorangebokeh.webm"
+"backgrounds/videos/glimmering.webm"
+"backgrounds/videos/glowingrings.webm"
+"backgrounds/videos/greencircles.webm"
+"backgrounds/videos/hues.webm"
+"backgrounds/videos/inthedeep.webm"
+"backgrounds/videos/modernflourish.webm"
+"backgrounds/videos/orangeaura.webm"
+"backgrounds/videos/warminside.webm"
+"backgrounds/videos/yellowgreen.webm"
+
+You can add custom backgrounds
+Just make sure it is at webm VP8 format */
+var l_background = "backgrounds/videos/glimmering.webm";
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them*/
+var l_bgImages = [
+	"backgrounds/images/Alice.jpg",
+];
+
+/*Random background images order?*/
+var l_bgImagesRandom = true;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = true;
+
+/*Display actual song's name?*/
+var l_musicDisplay = true;
+
+/*Music playlist
+Place how much Youtube IDs/.ogg you want*/
+var l_musicPlaylist = [
+	{youtube: "mOKqNxN4jWM", name: "DJ Snake - Middle (Audio) ft. Bipolar Sunshine"},
+	{youtube: "KDxJlW6cxRk", name: "Duke Dumont - Ocean Drive"},
+	{youtube: "BMaT2nGaEVg", name: "99 Souls - The Girl Is Mine"},
+{youtube: "t5747BhezKM", name: "Tez Cadey - Seve"},
+{youtube: "Ly7uj0JwgKg", name: "Flume - Never be like you"}
+];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 10;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+"Welcome to Wonderland RP!",
+"To call a staff member make sure to use to @ symbol in the chat!",
+"To start a party with your friends put !party",
+"Make sure to read the MOTD to know the rules for each job!",
+"Like this server? Join our steam group!",
+"WonderlandDRP.enjin.com",
+"Want to become staff? Make sure you have all of the requirements!",
+"Donate to get your own custom weaponds, player model & much more!",
+"If you tell the owners a funny joke you will get 5k!:D",
+"Enjoy your stay at Wonderland Role Play!♥",
+"Remember to be kind and follow rules!"
+];
+
+/*Random message order?*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
